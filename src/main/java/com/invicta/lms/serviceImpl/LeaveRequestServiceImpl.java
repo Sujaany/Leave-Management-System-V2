c388c1cfa4128acbebb21f6ba0a5cd052ee976fe
ragulan22 <balasingamragulan@gmail.com>
2019-08-01 11:07:09 +0530

    leave request days setting

--- src/main/java/com/invicta/lms/serviceImpl/LeaveRequestServiceImpl.java
+++ src/main/java/com/invicta/lms/serviceImpl/LeaveRequestServiceImpl.java
@@ -2,18 +2,23 @@ package com.invicta.lms.serviceImpl;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.invicta.lms.dto.LeaveDtoRequest;
 import com.invicta.lms.dto.LeaveDtoResponse;
+import com.invicta.lms.dto.LeaveManagerDtoRequest;
 import com.invicta.lms.dto.LeaveProcessDtoRequest;
 import com.invicta.lms.dto.mapper.LeaveRequestMapper;
 import com.invicta.lms.entity.LeaveRequest;
+import com.invicta.lms.enums.LeaveProcessType;
 import com.invicta.lms.enums.LeaveRequestAction;
 import com.invicta.lms.enums.LeaveRequestStatus;
 import com.invicta.lms.repository.LeaveRequestRepository;
 import com.invicta.lms.repository.UserRepository;
+import com.invicta.lms.service.LeaveManagerService;
 import com.invicta.lms.service.LeaveRequestProcessService;
 import com.invicta.lms.service.LeaveRequestService;
 
@@ -28,6 +33,8 @@ public class LeaveRequestServiceImpl implements LeaveRequestService {
 	UserRepository userRepository;
 	@Autowired
 	LeaveRequestProcessService leaveRequestProcessService;
+	@Autowired
+	LeaveManagerService leaveManagerService;
 	
 	
 	private  LeaveRequest addLeaveRequest(LeaveDtoRequest leaveDtoRequest,Long requestUserId) {
@@ -75,10 +82,23 @@ public class LeaveRequestServiceImpl implements LeaveRequestService {
 	}
 
 	@Override
+	@Transactional
 	public Boolean createInitialLeaveRequestProcess(LeaveDtoRequest leaveDtoRequest,Long requestUserId) {
 		LeaveRequest leaveRequest=this.addLeaveRequest(leaveDtoRequest, requestUserId);
 		LeaveProcessDtoRequest leaveProcessDtoRequest=new LeaveProcessDtoRequest(LeaveRequestAction.APPLIED,"Leave Request applied");
-		leaveRequestProcessService.processLeaveRequest(leaveRequest.getId(),requestUserId,leaveProcessDtoRequest);
+		
+		LeaveManagerDtoRequest leaveManagerDtoRequest=new LeaveManagerDtoRequest();
+		leaveManagerDtoRequest.setLeaveProcessType(LeaveProcessType.UTILIZED);
+		leaveManagerDtoRequest.setLeaveTypeId(leaveDtoRequest.getLeaveType());
+		
+		Double numOfDays=(-1)*leaveDtoRequest.getNoOfDays();
+		leaveManagerDtoRequest.setDays(numOfDays);
+		leaveManagerService.addLeaveManager(requestUserId, leaveManagerDtoRequest);
+		
+		
+		if(leaveRequestProcessService.processLeaveRequest(leaveRequest.getId(),requestUserId,leaveProcessDtoRequest)!=null) {
+			return true;
+		}
 		
 		return false;
 	}
